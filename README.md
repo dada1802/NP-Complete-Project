761a7bf6d3de4145aeb666d06e97c21ccc382452
David Nguyen <nguy26dx@dukes.jmu.edu>
2023-04-24 11:18:59 -0400

    Added the problem description and the input format to the README file.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NP-Complete-Project
+
+David Nguyen will do the exact solution part.
+Christopher Simmons will do the approximation part.
+
+Problem description:
+    Given an undirected graph, find the minimum color needed to label each node such that there are no adjacent labels of the same color.
+
+Problem input format:
+    - The first line tells you how many vertices are in the graph G. The lines that follow contain the edge list for edge vertex.
--- exact_solution/main.py
+++ exact_solution/main.py
@@ -5,8 +5,20 @@
 import itertools
 
 def check_independent_set(nums, graph):
-    colors = len(nums) - 1
-    return True
+    check = True
+    colors = {}
+
+    for u in graph:
+        colors[u] = None
+        if u == 0:
+            colors[u] = 0
+
+    for u in graph:
+        for v in graph[u]:
+            if len(nums) < 2:
+                return False, colors
+
+    return check, colors
 
 
 def create_graph(n):
@@ -24,25 +36,39 @@ def create_graph(n):
 
 def main():
     # start_time = time.time()
-    
-    n = 2
+    # num_vertices = int(input())
+
+    # for i in range(num_vertices):
+    #     graph[i] = set()
+
+    # for _ in range(num_vertices):
+    #     x = input().split()
+    #     for i in range(1, len(x)):
+    #         graph[int(x[i])].add(int(x[0]))
+    #         graph[int(x[0])].add(int(x[i]))
+
+    n = 3
     num_colors = 1
     check = False
     graph = create_graph(n)
     print(graph)
 
-    # while not check:
-    #     nums = []
+    while not check:
+        nums = []
         
-    #     for i in range(num_colors):
-    #         nums.append(i)
+        for i in range(num_colors):
+            nums.append(i)
 
-    #     lst = itertools.product(nums, repeat=n)
+        lst = itertools.product(nums, repeat=n)
 
-    #     for val in lst:
-    #         print(val)
+        for val in lst:
+            print(val)
+        
+        num_colors += 1
+        print()
 
-    #     num_colors += 1
+        if num_colors == 3:
+            check = True
 
     # print("--- %s seconds ---" % (time.time() - start_time))
 
