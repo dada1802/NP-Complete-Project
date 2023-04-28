e790cbd52e5cf386034c196e0fb3f9c19a9531cf
David Nguyen <nguy26dx@dukes.jmu.edu>
2023-04-27 20:44:27 -0400

    Updated

--- exact_solution/run_test_cases.sh
+++ exact_solution/run_test_cases.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# for test_case in test_cases/incomplete_graphs/*; do
-#     echo "Running test case: $test_case"
-#     python main.py < "$test_case"
-#     echo
-# done
-
-for test_case in test_cases/complete_graphs/*; do
+for test_case in test_cases/*; do
     echo "Running test case: $test_case"
-    python complete_graphs.py < "$test_case"
+    python main.py < "$test_case"
     echo
 done
